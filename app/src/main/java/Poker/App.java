0a025042d5eb4333700d1b891e2aa826c3a26875
unknown <yarolsav.hontar@gmail.com>
2021-02-24 22:00:04 +0100

    moved all the functions to hand class

--- app/src/main/java/Poker/App.java
+++ app/src/main/java/Poker/App.java
@@ -9,53 +9,9 @@ import java.util.*;
 public class App {
 
     public static void main(String[] args) {
-        //TODO crete class cards which will convert card names to numbers and will sort cards
-        int i = 0;
-        int counter = 0;
-        String result = "";
-
-        Multimap<String, String> valuesMap = ArrayListMultimap.create();
         Hand hand1 = new Hand(args[0]);
-        result = hand1.handCombo();
+        String result = hand1.getHighestCombo();
 
-
-        // converting Collection of strings to arrayList of integers and sorting for comparison
-
-        // checking if cards in order
-        if(colorsMap.keySet().size() == 1){
-            int lastI = cValues.size() -1;
-            counter = LaMain.valuesOrderCheck(cValues);
-            if(counter > 0){
-                result = "flush";
-            }else if(cValues.get(lastI) == 14 ){
-                result = "La quinte flush royale";
-            }else  result = "La quinte flush";
-
-        } else {
-            int value = 0;
-            switch (valuesMap.keySet().size()){
-                case 2:
-                    value = LaMain.sameValuesCheck(valuesMap);
-                    result = (value == 0) ?"Le carre" : "Le full";
-                    break;
-                case 3:
-                    value = LaMain.sameValuesCheck(valuesMap);
-                    result = (value == 2) ? "La double paire" : " Le brelan";
-                    break;
-                case 4:
-                    result = "La paire";
-                    break;
-                default:
-                    System.out.println(cValues);
-                    counter = LaMain.valuesOrderCheck(cValues);
-                    System.out.println(counter);
-                    if(counter == 0){
-                        result = "La suite";
-                    } else{
-                        result  = "La carte haute";
-                    }
-            }
-        }
         System.out.println(result);
     }
 }
--- app/src/main/java/Poker/Hand.java
+++ app/src/main/java/Poker/Hand.java
@@ -9,34 +9,32 @@ import java.util.Collections;
 
 public class Hand {
     private String hand;
-    private Multimap<String, String> colorsMap;
-    private Multimap<String, String> valuesMap;
-    private ArrayList<Integer> cValues;
+    String result;
+    int counter;
+    private Multimap<String, String> colorsMap = ArrayListMultimap.create();
+    private Multimap<String, String> valuesMap = ArrayListMultimap.create();
+    private ArrayList<Integer> cValues = new ArrayList<Integer>();
+
 
     public Hand(String args) {
         this.hand = args;
-        this.colorsMap = ArrayListMultimap.create();
-        this.valuesMap = ArrayListMultimap.create();
-        this.cValues = new ArrayList<Integer>();
+        this.result = "";
+        this.counter = 0;
     }
 
     public String getHand() {
         return hand;
     }
 
-    public String handCombo() {
-        String combo = "";
-        return combo;
-    }
 
-    private String[] handFormatting(String args) {
-        String arg = args.replaceAll("-", "");
+    public String[] handFormatting() {
+        String arg = this.hand.replaceAll("-", "");
         String[] s = arg.split("");
         return s;
     }
 
-    private String[] cardNamesToNumbers() {
-        String[] s = this.handFormatting(this.hand);
+    public String[] cardNamesToNumbers() {
+        String[] s = this.handFormatting();
         int j = 0;
         while (j < s.length) {
             switch (s[j]) {
@@ -63,31 +61,90 @@ public class Hand {
         return s;
     }
 
-    private Multimap<String, String> createColorsMap() {
+    public void createColorsMap() {
         String[] string  = cardNamesToNumbers();
         int i = 0;
         while (i < string.length) {
-            colorsMap.put(string[i + 1], string[i]);
+            this.colorsMap.put(string[i + 1], string[i]);
+            i += 2;
         }
-        return colorsMap;
     }
 
-    private Multimap<String, String> createValuesMap() {
+    private void createValuesMap() {
         String[] string  = cardNamesToNumbers();
         int i = 0;
         while (i < string.length) {
-            valuesMap.put(string[i], string[i+1]);
+            this.valuesMap.put(string[i], string[i+1]);
             i +=2;
         }
-        return valuesMap;
     }
+
    // converting Collection of strings to arrayList of integers and sorting for comparison
-    private ArrayList<Integer> sortValuesAscending(){
-        Collection<String> cardValues = colorsMap.values();
+    private void sortValuesAscending(){
+        Collection<String> cardValues = this.colorsMap.values();
         for(String numeric : cardValues)
         {
-            cValues.add(Integer.parseInt(numeric));
+            this.cValues.add(Integer.parseInt(numeric));
         }
-        return cValues;
+        Collections.sort(this.cValues);
+
+    }
+
+    private int sameValuesCheck(){
+        int value = 0;
+        for(String key: valuesMap.keySet()){
+            Collection values = valuesMap.get(key);
+            if (values.size() == 2) value = 2;
+        }
+        return value;
+    }
+
+    private int valuesOrderCheck(){
+        sortValuesAscending();
+        counter = 0;
+        for(int i = 1; i < cValues.size(); i++){
+            if(cValues.get(i) != (cValues.get(i-1) + 1)) counter++;
+            if(counter > 0) i = cValues.size();
+        }
+
+        return counter;
+    }
+
+    public String getHighestCombo(){
+        createColorsMap();
+        createValuesMap();
+        counter = valuesOrderCheck();
+        int lastI = this.cValues.size() -1;
+        if(colorsMap.keySet().size() == 1){
+            if(counter > 0){
+                result = "flush";
+            }else if(cValues.get(lastI) == 14 ){
+                result = "La quinte flush royale";
+            }else  result = "La quinte flush";
+        } else {
+            int value = 0;
+            switch (valuesMap.keySet().size()){
+                case 2:
+                    value = sameValuesCheck();
+                    result = (value == 0) ?"Le carre" : "Le full";
+                    break;
+                case 3:
+                    value = sameValuesCheck();
+                    result = (value == 2) ? "La double paire" : " Le brelan";
+                    break;
+                case 4:
+                    result = "La paire";
+                    break;
+                default:
+                    //System.out.println(cValues);
+                    counter = valuesOrderCheck();
+                    if(counter == 0){
+                        result = "La suite";
+                    } else{
+                        result  = "La carte haute";
+                    }
+            }
+        }
+        return result;
     }
 }
